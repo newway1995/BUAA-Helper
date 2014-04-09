4c30cf3f2a0175cab9282d931f49c08fd4010d25
chenyanming <chenym@chenyanmingtekiMac-mini.local>
2014-04-09 10:12:08 +0800

    Initial Commit

---
+++ AnimatTabbarSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimatTabbarSample
+//
+//  Created by chenyanming on 14-4-9.
+//  Copyright (c) 2014年 chenyanming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnimatTabbarSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnimatTabbarSample
+//
+//  Created by chenyanming on 14-4-9.
+//  Copyright (c) 2014年 chenyanming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
