11f65d1776092b92d410cb04b6ad615395b071ff
GYX <buaagyx2012@gmail.com>
2015-08-05 22:09:21 +0800

    frame of the program & a simple map

---
+++ AnimatTabbarSample/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  AnimatTabbarSample
+//
+//  Created by !n on 15/8/5.
+//  Copyright (c) 2015年 chenyanming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
